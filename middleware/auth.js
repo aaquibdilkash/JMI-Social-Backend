37ca91288a7dabdc1279d5e06378de180e09575d
Aaquib Dilkash <aaquibdilkash@gmail.com>
2021-10-23 13:27:14 +0530

    make it better

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken"
  const auth = async (req, res, next) => {
     try {
+        console.log(req.headers.authorization, "%%%%%%%%%%%%%%%%%%%%%%%%%%%")
         const token = req.headers.authorization.split(" ")[1]
         const isCustomAuth = token.length < 500
 
